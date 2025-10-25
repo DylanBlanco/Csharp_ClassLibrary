11864e3a69429e0516461a193ba102389bc39007
DylanBlanco <dylanblanco1198@gmail.com>
2025-10-25 17:15:46 +0200

    gioco - Dopo la vittoria, chiedi se vuole giocare di nuovo

--- Csharp_ClassLibrary/Program.cs
+++ Csharp_ClassLibrary/Program.cs
@@ -7,43 +7,56 @@ namespace Csharp_ClassLibrary
         // CREA ARRAY DI 10 NUM GENERATI CASUALMENTE E CHIEDI ALL'UTENTE DI INDOVINARE DUE NUMERI
         public static void Main(string[] args)
         {
-            Random rnd = new Random();
-            int[] numeriCasuali = new int[10];
+            bool continuaGioco = true;
 
-            // genera i numeri casuali
-            for (int i = 0; i < numeriCasuali.Length; i++)
+            while (continuaGioco)
             {
-                numeriCasuali[i] = rnd.Next(1, 11);
-            }
+                Random rnd = new Random();
+                int[] numeriCasuali = new int[10];
 
-            Console.WriteLine("Numeri generati: " + string.Join(", ", numeriCasuali));
-
-            while (true)
-            {
-                // chiedi i due numeri all'utente
-                Console.Write("Indovina il primo numero (1-10): ");
-                int num1 = int.Parse(Console.ReadLine());
-
-                Console.Write("Indovina il secondo numero (1-10): ");
-                int num2 = int.Parse(Console.ReadLine());
-
-                // controlla se sono presenti
-                bool trovato1 = numeriCasuali.Contains(num1);
-                bool trovato2 = numeriCasuali.Contains(num2);
-
-                // mostra il risultato
-                if (trovato1 && trovato2)
+                // genera i numeri casuali
+                for (int i = 0; i < numeriCasuali.Length; i++)
                 {
-                    Console.WriteLine("Hai indovinato entrambi i numeri!");
-                    break;
+                    numeriCasuali[i] = rnd.Next(1, 11);
                 }
-                else if (trovato1 || trovato2)
-                    Console.WriteLine("Hai indovinato solo uno dei due numeri!");
-                else
-                    Console.WriteLine("Non hai indovinato nessuno dei numeri!");
-            }
 
-            Console.WriteLine("Fine del gioco. Grazie per aver giocato!");
+                Console.WriteLine("Numeri generati: " + string.Join(", ", numeriCasuali));
+
+                while (true)
+                {
+                    // chiedi i due numeri all'utente
+                    Console.Write("Indovina il primo numero (1-10): ");
+                    int num1 = int.Parse(Console.ReadLine());
+
+                    Console.Write("Indovina il secondo numero (1-10): ");
+                    int num2 = int.Parse(Console.ReadLine());
+
+                    // controlla se sono presenti
+                    bool trovato1 = numeriCasuali.Contains(num1);
+                    bool trovato2 = numeriCasuali.Contains(num2);
+
+                    // mostra il risultato
+                    if (trovato1 && trovato2)
+                    {
+                        Console.WriteLine("Hai indovinato entrambi i numeri!");
+                        break;
+                    }
+                    else if (trovato1 || trovato2)
+                        Console.WriteLine("Hai indovinato solo uno dei due numeri!");
+                    else
+                        Console.WriteLine("Non hai indovinato nessuno dei numeri!");
+                }
+
+                // 🔹 Dopo la vittoria, chiedi se vuole giocare di nuovo
+                Console.Write("\nVuoi giocare di nuovo? (s/n): ");
+                string risposta = Console.ReadLine().ToLower();
+
+                if (risposta != "s" && risposta != "si")
+                {
+                    continuaGioco = false;
+                    Console.WriteLine("Fine del gioco. Grazie per aver giocato!");
+                }
+            }
         }
     }
 }
